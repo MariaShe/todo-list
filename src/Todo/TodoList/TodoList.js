d30ed2affd544e0b00bb526879ce6c772375b75a
Maria Shestakova <shestwork@gmail.com>
2020-09-01 13:41:03 +0300

    first commit

---
+++ src/Todo/TodoList/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './TodoList.module.css'
+import PropTypes from 'prop-types';
+import TodoItem from '../TodoItem/TodoItem';
+
+function TodoList(props) {
+    return (
+        <ul style={styles.ul}>
+            { props.todos.map((todo, index) => {
+                return <TodoItem 
+                todo={todo} 
+                key={todo.id} 
+                index={index} 
+                onChange={props.onToggle}
+                />
+            })}
+
+        </ul>
+    )
+}
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onToggle: PropTypes.func.isRequired
+}
+export default TodoList;
